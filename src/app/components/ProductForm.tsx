ebabb36611788a735cba4e39a4b102fbaf4f9727
Mastracci <38020285+MastracciN@users.noreply.github.com>
2026-05-03 21:07:19 -0400

    updated ProductForm.tsx component to be a popout menu when Add Item Manually button is pressed

--- src/app/components/ProductForm.tsx
+++ src/app/components/ProductForm.tsx
@@ -3,6 +3,8 @@
 import { useState } from "react";
 
 export default function ProductForm() {
+    const [open, setOpen] = useState(false);
+
     const [formData, setFormData] = useState({
         title: "",
         brand: "",
@@ -77,58 +79,102 @@ export default function ProductForm() {
     }
 
     return (
-        <form
-            onSubmit={handleSubmit}
-            className="space-y-4 p-4"
-        >
-            <input
-                type="text"
-                name="title"
-                value={formData.title}
-                onChange={handleChange}
-                placeholder="Title"
-                className="rounded-lg border px-3 py-2"
-            />
-
-            <input
-                type="text"
-                name="brand"
-                value={formData.brand}
-                onChange={handleChange}
-                placeholder="Brand"
-                className="rounded-lg border px-3 py-2"
-            />
-
-            <input
-                type="text"
-                name="category"
-                value={formData.category}
-                onChange={handleChange}
-                placeholder="Category"
-                className="rounded-lg border px-3 py-2"
-            />
-
-            <input
-                type="number"
-                name="quantity"
-                value={formData.quantity}
-                onChange={handleChange}
-                min={1}
-                className="rounded-lg border px-3 py-2"
-            />
-
-            <button 
-                type="submit"
-                disabled={loading}
-                className="rounded-lg px-4 py-2 text-white bg-green-500"
+        <div>
+            <button
+                onClick={() => setOpen(true)}
+                className="bg-green-600 p-2 rounded-lg"
             >
-                {loading ? "Adding..." : "Add New Item"}
+                Add Item Manually
             </button>
 
-            {message && (
-                <p className="text-sm">{message}</p>
-            )}
+            <div
+                className={`fixed inset-0 z-50 bg-black/40  transition-opacity duration-300
+                    ${ open 
+                        ? "pointer-events-auto opacity-100" 
+                        : "pointer-events-none opacity-0"
 
-        </form>
+                    }`}
+                    onClick={() => setOpen(false)}
+            >
+            </div>
+
+            <div
+                className={`fixed right-0 top-0 z-50 flex h-full w-full max-w-lg transform 
+                    flex-col bg-[#1f1f1f] shadow-2xl transition-transform duration-300
+                ${ open 
+                    ? "translate-x-0" 
+                    : "translate-x-full"
+                }`}
+            >
+                <div className="p-4 text-2xl">
+                    Add Item
+                </div>
+                <form
+                    onSubmit={handleSubmit}
+                    className="space-y-4 p-4 flex flex-col"
+                >
+                    <input
+                        type="text"
+                        name="title"
+                        value={formData.title}
+                        onChange={handleChange}
+                        placeholder="Title"
+                        className="rounded-lg border px-3 py-2"
+                    />
+
+                    <input
+                        type="text"
+                        name="brand"
+                        value={formData.brand}
+                        onChange={handleChange}
+                        placeholder="Brand"
+                        className="rounded-lg border px-3 py-2"
+                    />
+
+                    <input
+                        type="text"
+                        name="category"
+                        value={formData.category}
+                        onChange={handleChange}
+                        placeholder="Category"
+                        className="rounded-lg border px-3 py-2"
+                    />
+
+                    <input
+                        type="number"
+                        name="quantity"
+                        value={formData.quantity}
+                        onChange={handleChange}
+                        min={1}
+                        className="rounded-lg border px-3 py-2"
+                    />
+
+                    <div className="flex space-x-2 py-2">
+                        <button 
+                            type="submit"
+                            disabled={loading}
+                            className="flex-1 rounded-lg py-2 text-white bg-green-500"
+                        >
+                            {loading ? "Adding..." : "Add New Item"}
+                        </button>
+
+                        <button
+                            className="flex-1 bg-red-600 rounded-lg"
+                            onClick={() => setOpen(false)}
+                        >
+                            Cancel
+                        </button>
+                    </div>
+
+
+                    {message && (
+                        <p className="text-sm">{message}</p>
+                    )}
+
+                </form>
+
+            </div>
+        </div>
+
     )
 }
