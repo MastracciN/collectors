c29cdd65fb6584612e14826f212a9f4e509eb9e3
Mastracci <38020285+MastracciN@users.noreply.github.com>
2026-05-11 12:41:52 -0400

    LogoutButton has LogOut icon next to sign out text

--- src/app/components/auth/LogoutButton.tsx
+++ src/app/components/auth/LogoutButton.tsx
@@ -1,12 +1,15 @@
 "use client";
 
 import { signOut } from "next-auth/react";
+import { LogOut } from "lucide-react";
 
 export default function LogoutButton() {
     return (
         <button 
-            className="py-2 cursor-pointer w-full"
-            onClick={() => signOut()}>
+            className="flex items-center gap-2 p-2 cursor-pointer w-full "
+            onClick={() => signOut()}
+        >
+            <LogOut size={20}/>
             Sign out
         </button>
     );
